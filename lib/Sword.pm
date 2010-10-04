d2b32f9241420d4bfb3192f1eb8dd57ee1d3f8ac
Sterling Hanenkamp <hanenkamp@cpan.org>
2010-10-04 15:15:43 -0500

    Adding documentation for the API

--- lib/Sword.pm
+++ lib/Sword.pm
@@ -1,8 +1,9 @@
 package Sword;
-use Moose;
+use strict;
+use warnings;
 
+use Sword::Manager;
 use Sword::Module;
-use Sword::XS;
 
 =head1 NAME
 
@@ -11,79 +12,69 @@ Sword - provides an interface to the Sword Open Source Bible Software API
 =head1 SYNOPSIS
 
   use Sword;
-
-  my $library = Sword->new;
-
-  if (grep { $_ eq 'ESV' } $library->modules) {
-      my $esv = $library->module('ESV');
-
-      my $verse = $esv->lookup_text('John 3:16');
+  use List::Util qw( first );
+  
+  my $library = Sword::Manager->new;
+  
+  print "Your library contains:\n\n";
+  for my $module (sort { $a->type cmp $b->type } @{ $library->modules }) {
+      print "    ", $module->type, ": ", $module->name, " - ", 
+  	  $module->description, "\n";
+  }
+  
+  print "\n";
+  
+  # Try a preferred list of Bibles...
+  my $bible = $library->get_module('ESV')
+           || $library->get_module('KJV');
+  
+  # Or find any Bible...
+  $bible = first { $_->type eq 'Biblical Texts' } @{ $library->modules };
+  
+  if ($bible) {
+      $bible->set_key('John 3:16'); # can us abbrevs, like jn3.16
+      my $verse = $bible->render_text;
       print "John 3:16: $verse\n";
+  }
+  
+  else {
+      print "No Bible was found in your library. You may need to open your Sword software to install one.\n";
+  }
+  
+  print "\n\n";
+  
+  my $dict = $library->get_module('WebstersDict');
+  if ($dict) {
+      $dict->set_key('dictionary');
+      my $description = $dict->description;
+      print "According to $description:\n\n";
+      print $dict->render_text, "\n";
   }
 
 =head1 DESCRIPTION
 
-B<EXPERIMENTAL>. The goal is to reproduce the Sword Engine API faithfully, but not rigidly, in Perl. This is really just the first foray into that direction. It simply provides just enough to do what's shown in the L</SYNOPSIS>, but future versions should do so much more and to get there, things will probably need to change a lot here.
+B<EXPERIMENTAL>. The goal is to reproduce the Sword Engine API in Perl. This is really just the first foray into that direction. It simply provides just enough to do what's shown in the L</SYNOPSIS> and perhaps a bit more. Future versions should do more, but the Perl API may change in the process, so be sure to check the F<Changes> file for each release for API compatibility.
 
-=head1 ATTRIBUTES
+This module exists for convenience and does nothing more than load the most commonly needed modules:
 
-=head2 library
+=over
 
-This is the internal C<SWMgr> pointer used to talk to the Sword Engine API.
+=item *
 
-=cut
+L<Sword::Manager>
 
-has library => (
-    is         => 'ro',
-    required   => 1,
-    lazy_build => 1,
-);
+=item *
 
-sub _build_library { Sword::XS::get_library() }
+L<Sword::Module>
 
-=head1 METHODS
-
-=head2 new
-
-  my $sword = Sword->new;
-
-Returns a L<Sword> object that can be used to access the modules in your library.
-
-=head2 modules
-
-  my @names = $sword->modules;
-
-Returns a list of module names.
-
-=cut
-
-sub modules { 
-    my $self = shift;
-    return Sword::XS::list_modules($self->library);
-}
-
-=head2 module
-
-  my Sword::Module $module = $sword->module($name);
-
-Returns a L<Sword::Module> object that can be used to refer to data in particular textual source.
-
-=cut
-
-sub module { 
-    my ($self, $name) = @_;
-    return Sword::Module->new(
-        library => $self, 
-        name    => $name,
-    );
-}
+=back
 
 =head1 SEE ALSO
+
+L<Sword::Manager>, L<Sword::Module>.
 
 Sword Project: L<http://www.crosswire.org/sword/index.jsp>
 
 =cut
-
-__PACKAGE__->meta->make_immutable;
 
 1;
--- lib/Sword/Module.pm
+++ lib/Sword/Module.pm
@@ -9,3 +9,65 @@ XSLoader::load('Sword', $Sword::Module::VERSION);
 
 1;
 
+__END__
+
+=head1 NAME
+
+Sword::Module - Sword modules (Bibles, commentaries, dictionaries, etc.)
+
+=head1 SYNOPSIS
+
+  use Sword;
+
+  my $library = Sword::Manager->new;
+
+  my $module = $library->get_module('KJV');
+  $module->set_key('jn3.16');
+  print "John 3:16 says (KJV): ", $module->render_text, "\n";
+
+=head1 DESCRIPTION
+
+This Perl module provides access to the C<SWModule> class from the Sword Engine API.
+
+This documetnation should cover everything that you can do with it. If something is wrong or missing, please report a bug.
+
+=head1 METHODS
+
+=head2 name
+
+  my $name = $module->name;
+  $module->name($new_name);
+
+This is the getter/setter for the short name of the module.
+
+=head2 description
+
+  my $description = $module->description;
+  $module->description($new_description);
+
+This is the getter/setter for the long description of the module.
+
+=head2 type
+
+  my $type = $module->type;
+  $module->type($new_type);
+
+This is the getter/setter for the type of the module. This is the name of the category this module belongs to, like "Biblical Texts" or "Lexicons / Dictionaries".
+
+=head2 set_key
+
+  $module->set_key($key);
+
+This selects a key pointing into the text. The kind of key value used depends on the module. For example, in a Bible or commentary, this may be a scripture reference or an abbreviation of one like "John 3:16" or "Ps 23" or "rm8.28". In a dictionary, this may be a word or word number.
+
+=head2 render_text
+
+  my $str = $module->render_text;
+
+Returns the the text for the key that has been selected.
+
+=head1 SEE ALSO
+
+L<Sword::Manager>
+
+=cut
